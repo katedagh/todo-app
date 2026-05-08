99f9feaa4edb92f8ba7ebc5d907575c1f1a2b05a
Kateřina Daghbouj <katerina.daghbouj@gmail.com>
2026-05-08 20:09:22 +0200

    Save tasks to local storage

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const form = document.querySelector("#todo-form")
 
+ let toDoItems = []
+
 form.addEventListener("submit", event => {
     event.preventDefault()
 
@@ -8,8 +10,21 @@ form.addEventListener("submit", event => {
 
     } else {
 
+        toDoItems.push({
+            id: "",
+            toDo: event.target.elements.todo.value.trim(),
+            completion: false,
+        })
+
+        toDoItemsJSON = JSON.stringify(toDoItems)
+        localStorage.setItem("items", toDoItemsJSON)
+
+        let toDoItemsFromLS = localStorage.getItem("items")
+        let toDOItemsForList = JSON.parse(toDoItemsFromLS)
+
     let task = document.createElement("p")
-    task.textContent =  event.target.elements.todo.value.trim()
+    task.textContent =  toDOItemsForList[toDOItemsForList.length - 1].toDo
+    console.log(task)
     const toDoList = document.querySelector("#todo-list")
     toDoList.appendChild(task)
 
