641b63c04b3f7627ad8c329e281a8d94a0aefae2
Kateřina Daghbouj <katerina.daghbouj@gmail.com>
2026-05-08 19:25:41 +0200

    Add if condition for handling empty submit and trim spaces

--- script.js
+++ script.js
@@ -3,11 +3,17 @@ const form = document.querySelector("#todo-form")
 form.addEventListener("submit", event => {
     event.preventDefault()
 
-    let task = document.createElement("p")
-    task.textContent =  event.target.elements.todo.value
+    if (event.target.elements.todo.value === "") {
+       return
 
+    } else {
+
+    let task = document.createElement("p")
+    task.textContent =  event.target.elements.todo.value.trim()
     const toDoList = document.querySelector("#todo-list")
     toDoList.appendChild(task)
+
+    }
 
     event.target.elements.todo.value = ""
 })
