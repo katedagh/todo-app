f34379994804c356ebcf699f20b68ce530676618
Kateřina Daghbouj <katerina.daghbouj@gmail.com>
2026-05-08 20:18:10 +0200

    Add comments

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const form = document.querySelector("#todo-form")
+const toDoList = document.querySelector("#todo-list")
 
- let toDoItems = []
+let toDoItems = []
 
 form.addEventListener("submit", event => {
     event.preventDefault()
@@ -10,25 +11,26 @@ form.addEventListener("submit", event => {
 
     } else {
 
+        //push input text to object in array
         toDoItems.push({
             id: "",
             toDo: event.target.elements.todo.value.trim(),
             completion: false,
         })
 
+        //save items to local storage and get them out
         toDoItemsJSON = JSON.stringify(toDoItems)
         localStorage.setItem("items", toDoItemsJSON)
-
         let toDoItemsFromLS = localStorage.getItem("items")
         let toDOItemsForList = JSON.parse(toDoItemsFromLS)
-
-    let task = document.createElement("p")
-    task.textContent =  toDOItemsForList[toDOItemsForList.length - 1].toDo
-    console.log(task)
-    const toDoList = document.querySelector("#todo-list")
-    toDoList.appendChild(task)
+    
+         //create paragraph and add text to page
+        let task = document.createElement("p")
+        task.textContent =  toDOItemsForList[toDOItemsForList.length - 1].toDo
+        toDoList.appendChild(task)
 
     }
 
+    //clear input
     event.target.elements.todo.value = ""
 })
